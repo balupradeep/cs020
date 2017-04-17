1179a5c2a79abc854ad2dadc76cb816c0bc5cfe9
JidhuKrishnaC <jkrishna.c@gmail.com>
2017-04-17 15:01:06 +0530

    Update calc.sh

--- calc.sh
+++ calc.sh
@@ -2,15 +2,15 @@ sum=0
 i="y"
 while [ $i = "y" ]
 do
-echo "###ENTER THE FIRST NO:###"
+echo "### ENTER THE FIRST NO: ###"
 read n1
-echo "Enter second no:!!!!"
+echo "ENTER THE SECOND NO: "
 read n2
-echo "1.Addition#"
+echo "#1.Addition"
 echo "#2.Subtraction"
 echo "#3.Multiplication"
 echo "#4.Division"
-echo "###Enter your choice"
+echo "### ENTER YOUR CHOICE ##"
 read ch
 case $ch in
     1)sum=`expr $n1 + $n2`
@@ -21,9 +21,9 @@ case $ch in
      echo "Mul = "$sum;;
     4)sum=`expr $n1 / $n2`
      echo "Div = "$sum;;
-    *)echo "Invalid choice";;
+    *)echo "INVALID CHOICE";;
 esac
-echo "Do u want to continue ?"
+echo "Do u want to continue ?? ,then press 'y'!!"
 read i
 if [ $i != "y" ]
 then
